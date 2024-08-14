9349fa1b6db3d369bd8c72a581cebc2eea8d1d76
LukasElias <lukaslundmajland@gmail.com>
2024-08-14 16:48:06 +0200

    TODO: Finish the todo list

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/LukasElias/lazy-todo
 
 go 1.22.5
+
+require (
+	github.com/bit101/go-ansi v1.0.0
+)
--- main.go
+++ main.go
@@ -2,23 +2,47 @@ package main
 
 import (
 	"strings"
-	"errors"
 	"fmt"
 	"os"
+	"github.com/bit101/go-ansi"
 )
 
-func help() {
-	fmt.Print("Are you really asking for help? [Y/n] ")
+func new_todo() {
+	// TODO: Get this function finsihed so i can make todos that's not in the code 
+	fmt.Println("Good luck winning")
+	fmt.Println(`
+  |  |  
+--+--+--
+  |  |  
+--+--+--
+  |  |  `)
+}
+
+func ask(question string) (bool, error) {
+	fmt.Print(question + " [Y/n] ")
 	var input string
 
 	_, err := fmt.Scan(&input)
 	if err != nil {
-		fmt.Println(err)
-		return
+		return false, err
 	}
 
-	if strings.ToLower(input) != "y" {
-		return
+	if strings.ToLower(input) == "y" {
+		return true, nil
+	}
+
+	return false, nil
+}
+
+func help() (error) {
+	answer, err := ask("Are you really asking for help?")
+
+	if err != nil {
+		return err
+	}
+
+	if !answer {
+		return nil
 	}
 
 	fmt.Println(`
@@ -26,15 +50,29 @@ You really don't know how to use this thing???
 It's a todo app for people who are too lazy to set todos, so they play tic tac toe instead
 
 Commands:
-	new - This command creates a new todo IF you win or play draw in tic tac toe
-	delete - This command deletes a random todo, so lazy people like YOU don't have to do it
-	status - This command shows every todo you've created and actually haven't deleted yet`)
+	new <todo> - This command creates a new todo IF you win or play draw in tic tac toe
+	complete <todo> - This command completes a specified todo, but half the time it won't believe you and it deletes a random todo instead
+	status - This command shows every todo you've created and actually haven't finished yet, you lazy monster!`)
+
+	return nil
 }
 
 func main() {
 	if len(os.Args) <= 1 {
-		fmt.Println(errors.New("Not any arguments!!!!!!!!!!!"))
+		fmt.Println("So what am i supposed to-do (haha get it?) when you don't specify anything!!")
 		help()
 		os.Exit(0)
+	}
+
+	if os.Args[1] == "new" {
+		new_todo()
+	}
+
+	if os.Args[1] == "delete" {
+
+	}
+
+	if os.Args[1] == "status" {
+
 	}
 }
